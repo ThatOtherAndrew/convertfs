3df5f2b125884dba45633f9bb763956d343e9730
Andromeda <stroev.andrew@gmail.com>
2026-04-25 19:23:54 +0200

    Almost ready to run...

--- convertfs/main.py
+++ convertfs/main.py
@@ -1,14 +1,22 @@
 from pathlib import Path
 
+import pyfuse3
+import trio
 
-class ConvertFS:
-    def __init__(self, mount_dir: Path):
-        self.mount_dir = mount_dir
+from convertfs.converter import Converter
+
+
+class ConvertFS(pyfuse3.Operations):
+    def __init__(self, mount_dir: Path) -> None:
+        self.mount_dir = mount_dir.resolve()
         self.converters = []
 
-    def add_converter(self, converter):
+    def add_converter(self, converter: Converter) -> None:
         self.converters.append(converter)
 
-    def run(self):
-        # TODO
-        pass
+    def run(self) -> None:
+        pyfuse3.init(self, self.mount_dir.as_posix(), set(pyfuse3.default_options))
+        try:
+            trio.run(pyfuse3.main)
+        finally:
+            pyfuse3.close(unmount=True)
