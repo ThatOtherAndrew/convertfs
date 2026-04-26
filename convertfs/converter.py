1a09bc013f129a7ee368fc1a0ccc0dd9478a7fa1
Andromeda <stroev.andrew@gmail.com>
2026-04-26 02:05:00 +0200

    Expand converter ABC

--- convertfs/converter.py
+++ convertfs/converter.py
@@ -1,8 +1,13 @@
+from abc import ABC, abstractmethod
 from pathlib import Path
 from re import Pattern
 
 
-class Converter:
+class Converter(ABC):
     INPUTS: tuple[Pattern, ...] = ()
     OUTPUT_FILES: tuple[Path, ...] = ()
     OUTPUT_DIRS: tuple[Path, ...] = ()
+
+    @abstractmethod
+    def process(self, source: Path, requested: Path) -> bytes:
+        pass
